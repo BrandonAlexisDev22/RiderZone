390b651ecbb08149e25fbf204f83d8e401e9cca4
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-03-26 21:57:22 -0500

    Agregando mensaje de bienvenida a los usuarios

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const mensajeBienvenida = `
+    ${alert("Estaremos actualizando nuestra pagina continuamente, gracias por su atencion att: Brandon Alexis y Anderson Cano")}
+`
+
+window.onload = (() => {
+    mensajeBienvenida
+})
