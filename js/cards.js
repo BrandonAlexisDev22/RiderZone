30dc37fce097503c549451111ed98b210ab456e4
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-03-30 17:43:07 -0500

    Creando la clase principal para crear cada moto dinamicamente

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,24 @@
-//CREANDO CONTENEDORES PRINCIPALES DE LA SECCION
-const root = document.querySelector(".root")
-const container = document.createElement("div")
-const productContainer = document.createElement("div")
-const productCard = document.createElement("div")
-const productInfo = document.createElement("div")
+class Moto {
+  static id = 0;
+
+  constructor(modelo, marca, imagen, precio) {
+    this.id = Moto.id++;
+    this.modelo = modelo;
+    this.marca = marca;
+    this.imagen = imagen;
+    this.precio = precio;
+  }
+
+  static CrearElemento() {
+    return
+  }
+}
+
+const root = document.querySelector(".root");
+const container = document.createElement("div");
+container.classList.add("root-products");
+const productContainer = document.createElement("div");
+productContainer.classList.add("product-container");
+
+root.appendChild(container);
+container.appendChild(productContainer);
