b03694b2346ea560af4a06ddcd62d17a77d18e05
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-03-30 17:24:47 -0500

    Creando contenedores principales de la seccion

---
+++ js/cards.js
@@ -0,0 +1,6 @@
+//CREANDO CONTENEDORES PRINCIPALES DE LA SECCION
+const root = document.querySelector(".root")
+const container = document.createElement("div")
+const productContainer = document.createElement("div")
+const productCard = document.createElement("div")
+const productInfo = document.createElement("div")
