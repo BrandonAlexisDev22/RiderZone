89aba9b592f295f06a2fdacaa745eb1759819bed
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-04-02 20:26:41 -0500

    Agregando funcion que agrega dinamicamente los componentes cuando la pagina cargue

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@
 // window.onload = (() => {
 //     mensajeBienvenida
 // })
+
+// FUNCION DEL HEADER
+function loadComponent(containerId, modulePath) {
+    fetch(modulePath)
+        .then(response => response.text())
+        .then(data => document.querySelector(containerId).innerHTML = data)
+        .catch(err => console.log(err));
+}
+
+document.addEventListener("DOMContentLoaded",() => {
+    loadComponent(".header-container","../components/header.html")
+    loadComponent(".footer-content", "../components/footer.html")
+    loadSheet("../css/header.css")
+    loadSheet("../css/footer.css")
+})
+
+function loadSheet(href){
+    let link = document.createElement("link")
+    link.rel = "stylesheet"
+    link.href = href
+    document.head.appendChild(link)
+}
